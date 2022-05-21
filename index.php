5351da72c26314ceba00ba9629bd3dc3bf906f9d
Edvin Lindahl <fete.edvin@gmail.com>
2022-05-21 17:29:06 +0200

    format text

--- index.php
+++ index.php
@@ -7,22 +7,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/modal.css">
 </head>
 
 <body>
     <div class="wrapper">
-        <header class="parent">
-            <img src="images/golem-bg.jpg" class="background" alt="background" width="768" height="auto">
+        <header>
+            <img src="images/golem-bg.jpg" class="background" alt="background" width="500" height="auto">
             <div class="title-position">
                 <h1 class="title">GOLEM</h1>
             </div>
         </header>
         <div class="text-body">
             <section>
-                <p>A golem (/ˈɡoʊləm/ GOH-ləm; Hebrew: גּוֹלֶם‎, gōlem) is an animated anthropomorphic being in Jewish folklore which is entirely created from inanimate matter (usually clay or mud). In the Psalms and medieval writings, the word golem was used as a term for an amorphous, unformed material.[1]</p>
-                <p>The most famous golem narrative involves Judah Loew ben Bezalel, the late-16th-century rabbi of Prague. Many tales differ on how the golem was brought to life and controlled. According to Moment magazine, "the golem is a highly mutable metaphor with seemingly limitless symbolism. It can be a victim or villain, Jew or non-Jew, man or woman—or sometimes both. Over the centuries, it has been used to connote war, community, isolation, hope, and despair."[2]</p>
-                <p>The oldest stories of golems date to early Judaism. In the Talmud (Tractate Sanhedrin 38b), Adam was initially created as a golem (גולם) when his dust was "kneaded into a shapeless husk". Like Adam, all golems are created from mud by those close to divinity, but no anthropogenic golem is fully human. Early on, the main disability of the golem was its inability to speak. Sanhedrin 65b describes Rava creating a man (gavra). He sent the man to Rav Zeira. Rav Zeira spoke to him, but he did not answer. Rav Zeira said, "You were created by the sages; return to your dust" </p>
+                    <div>
+                        <p>A golem (/ˈɡoʊləm/ GOH-ləm; Hebrew: גּוֹלֶם‎, gōlem) is an animated anthropomorphic being in Jewish folklore which is entirely created from inanimate matter (usually clay or mud). In the Psalms and medieval writings, the word golem was used as a term for an amorphous, unformed material.[1]</p>
+                    </div>
+                    <div>
+                        <img class="inline-image" src="images/golem-third.png" alt="Picture of Golem" width="700px" height="auto">
+                    </div>
+                    <div>
+                        <p>The most famous golem narrative involves Judah Loew ben Bezalel, the late-16th-century rabbi of Prague. Many tales differ on how the golem was brought to life and controlled. According to Moment magazine, "the golem is a highly mutable metaphor with seemingly limitless symbolism. It can be a victim or villain, Jew or non-Jew, man or woman—or sometimes both. Over the centuries, it has been used to connote war, community, isolation, hope, and despair."[2]</p>
+                    </div>
+                    <div>
+                        <img class="inline-image" src="images/golem-fourth.jpg" alt="Picture of Golem" width="700px" height="auto">
+                    </div>
+                    <div>
+                        <p>The oldest stories of golems date to early Judaism. In the Talmud (Tractate Sanhedrin 38b), Adam was initially created as a golem (גולם) when his dust was "kneaded into a shapeless husk". Like Adam, all golems are created from mud by those close to divinity, but no anthropogenic golem is fully human. Early on, the main disability of the golem was its inability to speak. Sanhedrin 65b describes Rava creating a man (gavra). He sent the man to Rav Zeira. Rav Zeira spoke to him, but he did not answer. Rav Zeira said, "You were created by the sages; return to your dust" </p>
+                    </div>
             </section>
+        </div>
+        <div class="size-test">
+
         </div>
         <div class="second-image">
             <img src="images/the-golem.jpg" class="background" alt="background" width="768" height="auto">
@@ -31,10 +47,10 @@
             <div class="footer">
                 <footer>
                     <p>
-                        For more information about the Golem, visit <a href="https://images.justwatch.com/backdrop/241272046/s640/the-golem-1920">his lair.</a>
+                        For more information about Golem, visit <a href="https://images.justwatch.com/backdrop/241272046/s640/the-golem-1920">his lair.</a>
                     </p>
                     <p class="rights-box">
-                        © 2022 All Rights Reserved
+                        © 2022 Fetus Lanus. All Rights Reserved
                     </p>
                 </footer>
             </div>
