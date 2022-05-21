1b4c407ef43da31df7f47e85d0160e29243e2fb9
Edvin Lindahl <fete.edvin@gmail.com>
2022-05-21 05:25:43 +0200

    parallax golem

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <header>
+            <img src="images/golem-bg.jpg" class="background" alt="background">
+            <h1 class="title">GOLEM</h1>
+        </header>
+        <div class="text-body">
+            <section>
+                A golem (/ˈɡoʊləm/ GOH-ləm; Hebrew: גּוֹלֶם‎, gōlem) is an animated anthropomorphic being in Jewish folklore which is entirely created from inanimate matter (usually clay or mud). In the Psalms and medieval writings, the word golem was used as a term for an amorphous, unformed material.[1]
+                <br>
+                <br>
+                The most famous golem narrative involves Judah Loew ben Bezalel, the late-16th-century rabbi of Prague. Many tales differ on how the golem was brought to life and controlled. According to Moment magazine, "the golem is a highly mutable metaphor with seemingly limitless symbolism. It can be a victim or villain, Jew or non-Jew, man or woman—or sometimes both. Over the centuries, it has been used to connote war, community, isolation, hope, and despair."[2]
+            </section>
+        </div>
+    </div>
+</body>
+
+</html>
