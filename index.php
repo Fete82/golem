f08d5631e5ede8dce62e9ac21c39064ec5d5cf40
Edvin Lindahl <fete.edvin@gmail.com>
2022-05-21 05:49:30 +0200

    footer with link

--- index.php
+++ index.php
@@ -19,11 +19,17 @@
         </header>
         <div class="text-body">
             <section>
-                A golem (/ˈɡoʊləm/ GOH-ləm; Hebrew: גּוֹלֶם‎, gōlem) is an animated anthropomorphic being in Jewish folklore which is entirely created from inanimate matter (usually clay or mud). In the Psalms and medieval writings, the word golem was used as a term for an amorphous, unformed material.[1]
-                <br>
-                <br>
-                The most famous golem narrative involves Judah Loew ben Bezalel, the late-16th-century rabbi of Prague. Many tales differ on how the golem was brought to life and controlled. According to Moment magazine, "the golem is a highly mutable metaphor with seemingly limitless symbolism. It can be a victim or villain, Jew or non-Jew, man or woman—or sometimes both. Over the centuries, it has been used to connote war, community, isolation, hope, and despair."[2]
+                <p>A golem (/ˈɡoʊləm/ GOH-ləm; Hebrew: גּוֹלֶם‎, gōlem) is an animated anthropomorphic being in Jewish folklore which is entirely created from inanimate matter (usually clay or mud). In the Psalms and medieval writings, the word golem was used as a term for an amorphous, unformed material.[1]</p>
+                <p>The most famous golem narrative involves Judah Loew ben Bezalel, the late-16th-century rabbi of Prague. Many tales differ on how the golem was brought to life and controlled. According to Moment magazine, "the golem is a highly mutable metaphor with seemingly limitless symbolism. It can be a victim or villain, Jew or non-Jew, man or woman—or sometimes both. Over the centuries, it has been used to connote war, community, isolation, hope, and despair."[2]
+                </p>
             </section>
+        </div>
+        <div class="footer">
+            <footer>
+                <p>
+                    For more information about the Golem, visit <a href="https://images.justwatch.com/backdrop/241272046/s640/the-golem-1920">The Golem</a>
+                </p>
+            </footer>
         </div>
     </div>
 </body>
