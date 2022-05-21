2b0c40cdcfdf3ceef96d78914600bbd77766f9b1
Edvin Lindahl <fete.edvin@gmail.com>
2022-05-21 17:45:37 +0200

    image resize

--- index.php
+++ index.php
@@ -24,13 +24,13 @@
                         <p>A golem (/ˈɡoʊləm/ GOH-ləm; Hebrew: גּוֹלֶם‎, gōlem) is an animated anthropomorphic being in Jewish folklore which is entirely created from inanimate matter (usually clay or mud). In the Psalms and medieval writings, the word golem was used as a term for an amorphous, unformed material.[1]</p>
                     </div>
                     <div>
-                        <img class="inline-image" src="images/golem-third.png" alt="Picture of Golem" width="700px" height="auto">
+                        <img class="inline-image" src="images/golem-third.png" alt="Picture of Golem" width="600px" height="auto">
                     </div>
                     <div>
                         <p>The most famous golem narrative involves Judah Loew ben Bezalel, the late-16th-century rabbi of Prague. Many tales differ on how the golem was brought to life and controlled. According to Moment magazine, "the golem is a highly mutable metaphor with seemingly limitless symbolism. It can be a victim or villain, Jew or non-Jew, man or woman—or sometimes both. Over the centuries, it has been used to connote war, community, isolation, hope, and despair."[2]</p>
                     </div>
                     <div>
-                        <img class="inline-image" src="images/golem-fourth.jpg" alt="Picture of Golem" width="700px" height="auto">
+                        <img class="inline-image" src="images/golem-fourth.jpg" alt="Picture of Golem" width="500px" height="auto">
                     </div>
                     <div>
                         <p>The oldest stories of golems date to early Judaism. In the Talmud (Tractate Sanhedrin 38b), Adam was initially created as a golem (גולם) when his dust was "kneaded into a shapeless husk". Like Adam, all golems are created from mud by those close to divinity, but no anthropogenic golem is fully human. Early on, the main disability of the golem was its inability to speak. Sanhedrin 65b describes Rava creating a man (gavra). He sent the man to Rav Zeira. Rav Zeira spoke to him, but he did not answer. Rav Zeira said, "You were created by the sages; return to your dust" </p>
