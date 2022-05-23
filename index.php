dc65ce30520e1330101cbb3758b0da1fd2101bb6
Edvin Lindahl <fete.edvin@gmail.com>
2022-05-23 14:58:35 +0200

    added info about api

--- index.php
+++ index.php
@@ -10,6 +10,14 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Odibee+Sans&display=swap" rel="stylesheet">
+
+    <!-- API INFO 
+
+    https://en.wikipedia.org/w/api.php?action=parse&page=golem&format=json&prop=sections
+
+    get all section titles.
+
+-->
 </head>
 
 <body>
@@ -24,13 +32,13 @@
         <div class="text-body">
             <section>
                 <div>
-                    <p>A golem (/ˈɡoʊləm/ GOH-ləm; Hebrew: גּוֹלֶם‎, gōlem) is an animated anthropomorphic being in Jewish folklore which is entirely created from inanimate matter (usually clay or mud). In the Psalms and medieval writings, the word golem was used as a term for an amorphous, unformed material.[1]</p>
+                    <p>A golem (/ˈɡoʊləm/ GOH-ləm; Hebrew: גּוֹלֶם‎, gōlem) is an animated anthropomorphic being in Jewish folklore which is entirely created from inanimate matter (usually clay or mud). In the Psalms and medieval writings, the word golem was used as a term for an amorphous, unformed material.</p>
                 </div>
                 <div>
                     <img class="inline-image" src="images/golem-third-2.png" alt="Picture of Golem" width="600px" height="auto">
                 </div>
                 <div>
-                    <p>The most famous golem narrative involves Judah Loew ben Bezalel, the late-16th-century rabbi of Prague. Many tales differ on how the golem was brought to life and controlled. According to Moment magazine, "the golem is a highly mutable metaphor with seemingly limitless symbolism. It can be a victim or villain, Jew or non-Jew, man or woman—or sometimes both. Over the centuries, it has been used to connote war, community, isolation, hope, and despair."[2]</p>
+                    <p>The most famous golem narrative involves Judah Loew ben Bezalel, the late-16th-century rabbi of Prague. Many tales differ on how the golem was brought to life and controlled. According to Moment magazine, "the golem is a highly mutable metaphor with seemingly limitless symbolism. It can be a victim or villain, Jew or non-Jew, man or woman—or sometimes both. Over the centuries, it has been used to connote war, community, isolation, hope, and despair."</p>
                 </div>
                 <div>
                     <img class="inline-image" src="images/golem-fourth.jpg" alt="Picture of Golem" width="500px" height="auto">
